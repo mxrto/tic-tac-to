49f2462208042cbf6e71d9136f78f1b0fb44f2b3
mxrto <raitondesu@gmail.com>
2021-07-17 15:36:21 +0200

    reworked main.py

--- main.py
+++ main.py
@@ -1,60 +1,61 @@
 #tic-tac-to
 import random
-
-
-def com_rand():
-    com_choice = [random.randint(0, 2), random.randint(0, 2)]
-    return com_choice
-
-
-def user_input():
-    user = input("Enter your row and line: ")
-    user_choice = []
-
-    for _ in user:
-        user_choice.append(int(_))
-    
-    return user_choice
-
-def check_win(i):
- 
-    for item in row1:
-        if item != i:
-            return True
-
-    return False
+import time
 
 def print_scoreboard():
-    for row in rows:
-        print(f"\n{row}")     
+    print(f"{game_board[0]}  {game_board[1]}  {game_board[2]}\n{game_board[3]}  {game_board[4]}  {game_board[5]}\n{game_board[6]}  {game_board[7]}  {game_board[8]}")    
+
+def check_win(board, i):
+    if board[0] == i and board[1] == i and board[2] == i:
+        return False
+
+    elif board[3] == i and board[4] == i and board[5] == i:
+        return False
+
+    elif board[6] == i and board[7] == i and board[8] == i:
+        return False
+
+    elif board[0] == i and board[3] == i and board[6] == i:
+        return False
+
+    elif board[1] == i and board[4] == i and board[7] == i:
+        return False
+
+    elif board[2] == i and board[5] == i and board[8] == i:
+        return False
     
+    elif board[0] == i and board[4] == i and board[8] == i:
+        return False
 
-row1 = ["_", "_", "_"]
-row2 = ["_", "_", "_"]
-row3 = ["_", "_", "_"]
+    elif board[2] == i and board[4] == i and board[6] == i:
+        return False
+    else:
+        return True
 
-rows = [row1, row2, row3]
+def rand_com():
+    return random.randint(0, 8)
+
+
+game_board = ["_", "_", "_", "_", "_", "_", "_", "_", "_"]
 
 game_is_on = True
-
 while game_is_on:
+    user = int(input("Enter a location: "))
+    com = rand_com()
 
-
-    com_choice = com_rand()
-    user_choice = user_input()
-
-    if "_" in rows[user_choice[0] - 1][user_choice[1] - 1]:
-        rows[user_choice[0] - 1][user_choice[1] - 1] = "X"
-        game_is_on = check_win(i="X")
-        
+    game_board[user] = "X"
+    game_is_on = check_win(game_board, "X")
     print_scoreboard()
 
-    if game_is_on == False:
-        break
+    print("\n")
 
-    if "_" in rows[com_choice[0]][com_choice[1]]:
-        rows[com_choice[0]][com_choice[1]] = "0"
-        game_is_on = check_win(i="0")
+    while game_board[com] != "_":
+        com = rand_com()
+    
+    game_board[com] = "0"
+    game_is_on = check_win(game_board, "0")
+    print_scoreboard()
+
 
 
 
